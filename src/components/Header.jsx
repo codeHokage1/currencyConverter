7c6b41fb3df7cc2a9921b92c047393d7aa5a226c
Anagho okiroro Obruthe <anaghosamroyce@gmail.com>
2022-07-31 08:11:06 +0100

    Responsive CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,15 @@ import Logo from "../images/pwc.png"
 
 const Header = () => {
     return (
-        <header className='container'>
-            <div>
-                <img src={Logo} alt="" className='logo' />
-                <span>Currency Converter</span>
-            </div>
-            <nav>
-                <ul>
-                    <li></li>
-                </ul>
-            </nav>
 
-        </header>
+        <div className='container'>
+            <header>
+                <div>
+                    <img src={Logo} alt="" className='logo' />
+                    <span>Currency Converter</span>
+                </div>
+            </header >
+        </div >
     )
 }
 
--- src/features/Converter.js
+++ src/features/Converter.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
-// import { US } from 'country-flag-icons/react/3x2'
+// Flag Icons
 import { FlagIcon } from "react-flag-kit";
-
+import CountryData from '../components/CountryData';
 
 const Converter = () => {
 
@@ -11,66 +11,69 @@ const Converter = () => {
 
         <div>
             <Header />
-            <div className='container converter-main'>
+            <div className='container converter-wrapper'>
 
                 <section>
                     <h3>All-In-One Currency Converter</h3>
-                    <section className='card grid'>
-                        <div className='converter-top'>
+
+                    <section className='card converter-main'>
+                        <div className='converter-left'>
                             <div className='top-left'>
                                 <small>You Have</small>
                                 {/* Dynamically pass in the country name here */}
                                 <small className='country'>Nigeria</small>
                             </div>
+                            <div className='body-left'>
+                                <div className='input-group'>
+                                    <input type='number' />
+
+                                    <select id='country'>
+                                        <option value='NG'>NG</option>
+                                        <option value='US'>US</option>
+                                        <option value=''></option>
+                                    </select>
+                                    {/* Pass a prop here code={country-code} to change flag dynamically */}
+                                    <FlagIcon className='flag' code="NG" width={18} height={15} />
+                                </div>
+                            </div>
+
+                            <div className='bottom-left'>
+                                <small>1USD</small>
+                                <small>=</small>
+                                <small>750.00</small>
+                            </div>
+                        </div>
+
+                        <div className='switch'>
+                            <i className="fa-solid fa-arrow-right-arrow-left"></i>
+                        </div>
+
+                        <div className='converter-right'>
                             <div className='top-right'>
                                 <small>You Get</small>
                                 {/* Dynamically pass in the country name here */}
                                 <small className='country'>United States</small>
                             </div>
-                        </div>
+                            <div className='body-right'>
+                                <div className='input-group'>
+                                    <input type='number' />
+                                    <select id='country'>
+                                        <option value='US'>US</option>
+                                        <option value='NGN'>NG</option>
+                                        <option value=''></option>
+                                    </select>
+                                    {/* Pass a prop here code={country-code} to change flag dynamically */}
+                                    <FlagIcon className='flag' code="US" width={18} height={15} />
+                                </div>
+                            </div>
 
-                        <div className='converter-body'>
-                            <div className='input-group'>
-                                <input type='number' />
-
-                                <select id='country'>
-                                    <option value='NG'>NG</option>
-                                    <option value='US'>US</option>
-                                    <option value=''></option>
-                                </select>
-                                {/* Pass a prop here code={country-code} to change flag dynamically */}
-                                <FlagIcon className='flag' code="NG" width={18} height={15} />
-                            </div>
-                            <div className='switch'>
-                                {/* <span>&arrow       right;</span> */}
-                                {/* <FaLongArrowAltRight color='white' />
-                                <FaLongArrowAltLeft color='white' /> */}
-                                <i className="fa-solid fa-arrow-right-arrow-left"></i>
-                            </div>
-                            <div className='input-group'>
-                                <input type='number' />
-                                <select id='country'>
-                                    <option value='US'>US</option>
-                                    <option value='NGN'>NG</option>
-                                    <option value=''></option>
-                                </select>
-                                {/* Pass a prop here code={country-code} to change flag dynamically */}
-                                <FlagIcon className='flag' code="US" width={18} height={15} />
-                            </div>
-                        </div>
-
-                        <div className='converter-bottom'>
-                            <div className='bottom-left'>
-                                <small>1USD</small>
-                                <small>=</small>
-                                <small>#750</small>
-                            </div>
                             <div className='bottom-right'>
                                 <small>1NGN</small>
                                 <small>=</small>
                                 <small>0.00133333USD</small>
                             </div>
                         </div>
+
                     </section>
                 </section>
 
